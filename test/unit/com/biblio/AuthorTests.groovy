dcc1ab6d12bb570ea443514f918f4b1333104bde
Ruben Salinas <elquesoy@gmail.com>
2010-08-20 12:50:17 +0200

    Simple test for Author.

--- test/unit/com/biblio/AuthorTests.groovy
+++ test/unit/com/biblio/AuthorTests.groovy
@@ -1,8 +1,7 @@
 package com.biblio
 
 import grails.test.*
-import com.biblio.Editorial
-import com.biblio.EditorialController 
+import com.biblio.Author
 
 class AuthorTests extends GrailsUnitTestCase {
     protected void setUp() {
@@ -14,10 +13,40 @@ class AuthorTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testEditorial() {
+    void testAuthor() {
     	Author a1 = new Author()
 		assertFalse a1.validate()
+
+		//Validación Author.nombre
 		assertEquals 'nullable', a1.errors['nombre']
+		a1.nombre = ""
+		assertFalse a1.validate()
+		assertEquals 'blank', a1.errors['nombre']
+		a1.nombre = "ab"
+		assertFalse a1.validate()
+		assertEquals 'size', a1.errors['nombre']
+		a1.nombre = "Francisco"
+		
+		//Validación Author.apellido1
 		assertEquals 'nullable', a1.errors['apellido1']
+		a1.apellido1 = ""
+		assertFalse a1.validate()
+		assertEquals 'blank', a1.errors['apellido1']
+		a1.apellido1 = "ab"
+		assertFalse a1.validate()
+		assertEquals 'size', a1.errors['apellido1']
+		a1.apellido1 = "Gómez"
+
+		//Validación Author.apellido2
+		a1.apellido2 = 'ab'
+		assertFalse a1.validate()
+		assertEquals 'size', a1.errors['apellido2']
+		a1.apellido2 = ''
+		assertTrue a1.validate()
+		a1.apellido2 = null
+		assertTrue a1.validate()
+		
+		a1 = new Author(nombre : 'Francisco', apellido1 : 'Gómez', apellido2 : 'de Quevedo')
+ 		assertTrue a1.validate()
     }
 }
